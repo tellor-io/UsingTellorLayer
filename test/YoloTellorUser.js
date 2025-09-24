5d17b36f3f3c1746d8d150f466392e1f4bfba467
tkernell <tpkernell@gmail.com>
2025-09-24 17:11:34 -0500

    domain sep updates, simplify testing

--- test/YoloTellorUser.js
+++ test/YoloTellorUser.js
@@ -5,27 +5,21 @@ const abiCoder = new ethers.AbiCoder();
 
 describe("YoloTellorUser - Function Tests", async function () {
 
-    let dataBridge, user, accounts, guardian, initialPowers, initialValAddrs, valCheckpoint;
-    let val1, val2, val3;
+    let dataBridge, user, accounts, guardian, validatorSet;
+    // for tellor mainnet, use "tellor-1"
+    // for tellor palmito testnet, use "layertest-4"
+    const TELLOR_CHAIN_ID = "layertest-4";
     const UNBONDING_PERIOD = 86400 * 7 * 3; // 3 weeks
 
     beforeEach(async function () {
         // init accounts
         accounts = await ethers.getSigners();
         guardian = accounts[10]
-        // init validator info
-        val1 = ethers.Wallet.createRandom()
-        val2 = ethers.Wallet.createRandom()
-        initialValAddrs = [val1.address, val2.address]
-        initialPowers = [1, 2]
-        threshold = 2
-        blocky = await h.getBlock()
-        valTimestamp = (blocky.timestamp - 2) * 1000
-        newValHash = await h.calculateValHash(initialValAddrs, initialPowers)
-        valCheckpoint = h.calculateValCheckpoint(newValHash, threshold, valTimestamp)
+        // init tellor validator set
+        validatorSet = await h.createTellorValset({tellorChainId: TELLOR_CHAIN_ID})
         // deploy dataBridge
-        dataBridge = await ethers.deployContract("TellorDataBridge", [guardian.address])
-        await dataBridge.init(threshold, valTimestamp, UNBONDING_PERIOD, valCheckpoint)
+        dataBridge = await ethers.deployContract("TellorDataBridge", [guardian.address, validatorSet.domainSeparator])
+        await dataBridge.init(validatorSet.powerThreshold, validatorSet.timestamp, UNBONDING_PERIOD, validatorSet.checkpoint)
         // deploy user
         user = await ethers.deployContract("YoloTellorUser", [dataBridge.getAddress()])
     });
@@ -35,11 +29,10 @@ describe("YoloTellorUser - Function Tests", async function () {
     })
 
     it("updateOracleData", async function () {
-        initialValidators = [val1, val2]
         querydata = abiCoder.encode(["string"], ["myquery"])
         queryId = h.hash(querydata)
         value = abiCoder.encode(["uint256"], [2000])
-        const { attestData, currentValidatorSet, sigs } = await h.prepareOracleData(queryId, value, initialValidators, initialPowers, valCheckpoint)
+        const { attestData, currentValidatorSet, sigs } = await h.prepareOracleData(queryId, value, validatorSet.wallets, validatorSet.powers, validatorSet.checkpoint)
         await user.updateOracleData(attestData, currentValidatorSet, sigs)
         currentOracleData = await user.getCurrentOracleData()
         assert.equal(currentOracleData.value, 2000)
