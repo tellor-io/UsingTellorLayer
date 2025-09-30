f8d88ba728a3facd51a3cdee2ce92497a3434b9d
tkernell <tpkernell@gmail.com>
2025-09-30 11:48:12 -0500

    updated yolo user

--- contracts/YoloTellorUser.sol
+++ contracts/YoloTellorUser.sol
@@ -3,36 +3,48 @@ pragma solidity 0.8.19;
 
 import "./interfaces/ITellorDataBridge.sol";
 
+// This contract shows a baseline oracle user that just verifies the data is valid tellor data
+// and stores the data for retrieval. It should never be used in production.
+
 contract YoloTellorUser {
     ITellorDataBridge public dataBridge;
+    bytes32 public queryId;
     OracleData[] public oracleData;
 
     struct OracleData {
-        uint256 value; // reported value
-        uint256 timestamp; // aggregate report timestamp
+        uint256 value;
+        uint256 timestamp;
     }
 
-    constructor(address _dataBridge) {
+    // sets the tellor data bridge address and the queryId
+    constructor(address _dataBridge, bytes32 _queryId) {
         dataBridge = ITellorDataBridge(_dataBridge);
+        queryId = _queryId;
     }
 
+    // updates this contract with new oracle data using the data bridge
+    // production users should add security checks
     function updateOracleData(
         OracleAttestationData calldata _attestData,
         Validator[] calldata _currentValidatorSet,
         Signature[] calldata _sigs
     ) external {
+        // make sure the data is valid tellor data
         dataBridge.verifyOracleData(_attestData, _currentValidatorSet, _sigs);
+        // make sure the data being relayed is the data we want
+        require(queryId == _attestData.queryId, "Invalid queryId");
+
+        // decode the data and store it
         uint256 _value = abi.decode(_attestData.report.value, (uint256));
-        oracleData.push(OracleData(
-            _value, 
-            _attestData.report.timestamp
-        ));
+        oracleData.push(OracleData(_value, _attestData.report.timestamp));
     }
 
-    function getCurrentOracleData() external view returns (OracleData memory) {
+    // returns the most recent data
+    function getCurrentData() external view returns (OracleData memory) {
         return oracleData[oracleData.length - 1];
     }
 
+    // returns the number of data points stored
     function getValueCount() external view returns (uint256) {
         return oracleData.length;
     }
--- test/YoloTellorUser.js
+++ test/YoloTellorUser.js
@@ -10,6 +10,9 @@ describe("YoloTellorUser - Function Tests", async function () {
     // for tellor palmito testnet, use "layertest-4"
     const TELLOR_CHAIN_ID = "layertest-4";
     const UNBONDING_PERIOD = 86400 * 7 * 3; // 3 weeks
+    const QUERY_DATA_ARGS = abiCoder.encode(["string", "string"], ["eth", "usd"])
+    const QUERY_DATA = abiCoder.encode(["string"], ["SpotPrice", QUERY_DATA_ARGS])
+    const QUERY_ID = ethers.keccak256(QUERY_DATA)
 
     beforeEach(async function () {
         // init accounts
@@ -21,7 +24,7 @@ describe("YoloTellorUser - Function Tests", async function () {
         dataBridge = await ethers.deployContract("TellorDataBridge", [guardian.address, validatorSet.domainSeparator])
         await dataBridge.init(validatorSet.powerThreshold, validatorSet.timestamp, UNBONDING_PERIOD, validatorSet.checkpoint)
         // deploy user
-        user = await ethers.deployContract("YoloTellorUser", [dataBridge.getAddress()])
+        user = await ethers.deployContract("YoloTellorUser", [dataBridge.getAddress(), QUERY_ID])
     });
 
     it("constructor", async function () {
@@ -29,12 +32,10 @@ describe("YoloTellorUser - Function Tests", async function () {
     })
 
     it("updateOracleData", async function () {
-        querydata = abiCoder.encode(["string"], ["myquery"])
-        queryId = h.hash(querydata)
         value = abiCoder.encode(["uint256"], [2000])
-        const { attestData, currentValidatorSet, sigs } = await h.prepareOracleData(queryId, value, validatorSet.wallets, validatorSet.powers, validatorSet.checkpoint)
+        const { attestData, currentValidatorSet, sigs } = await h.prepareOracleData(QUERY_ID, value, validatorSet.wallets, validatorSet.powers, validatorSet.checkpoint)
         await user.updateOracleData(attestData, currentValidatorSet, sigs)
-        currentOracleData = await user.getCurrentOracleData()
+        currentOracleData = await user.getCurrentData()
         assert.equal(currentOracleData.value, 2000)
     })
 })
